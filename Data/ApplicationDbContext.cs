39cd976e6849d19de6b0f3aa3577265217459192
ThienMenh <dinhthienmenh1505@gmail.com>
2024-08-09 21:54:16 +0700

    Add Identity, EF package

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,4 +9,20 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            var tableName = entityType.GetTableName();
+            if (tableName.StartsWith("AspNet"))
+            {
+                entityType.SetTableName(tableName.Substring(6));
+            }
+        }
+    }
 }
