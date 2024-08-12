96efb8e9654a5a4e4f7307ff17b8101fc6f94e2a
ThienMenh <dinhthienmenh1505@gmail.com>
2024-08-12 21:41:09 +0700

    Add identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MultilEcommer.Models;
 using MultilEcommer.Models.Contact;
 
 namespace MultilEcommer.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
