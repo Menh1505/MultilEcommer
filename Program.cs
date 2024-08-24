7fb9440d654b940a6dc79ecc1a87dd5b7ac03812
ThienMenh <dinhthienmenh1505@gmail.com>
2024-08-24 09:23:26 +0700

    Add send mail service

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ using MultilEcommer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddOptions();
+var mailsetting = builder.Configuration.GetSection("MailSettings");
+builder.Services.Configure<MailSettings>(mailsetting);
+builder.Services.AddSingleton<IEmailSender, SendMailService>();
+
 // Add builder.Services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
